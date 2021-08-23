656a65be287774f5823814a87e5da4b988a0cdfd
LeandroFredericce <leandrofredericce@hotmail.com>
2021-08-23 18:46:29 -0300

    Cabeçalho e Rodapé

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" >
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+    <link rel="stylesheet" type="text/css" href="css/estilo.css" media="screen" />
+    <title>Doces da Vovó - Doces Caseiros</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bitter:wght@300;400;600&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <img src="images/Logo.png" alt="Logo" class="logo">
+    </header>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Quem somos</li>
+            <li>Contato</li>
+            <li>Localização</li>
+        </ul>
+    </nav>
+    <section class="content">
+        Conteúdo
+    </section>
+
+    <footer >
+        Nome
+    </footer>
+    
+</body>
+</html>
